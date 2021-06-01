3dcb82445959fb5a2704659c62f0d5d4fc894fa3
João Soares <jsoareez@gmail.com>
2021-05-31 21:02:40 -0300

    save subscriptions into faunadb

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,8 +1,8 @@
-/* eslint-disable no-restricted-syntax */
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,12 +42,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log({ event });
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failed.' });
+      }
     }
 
-    res.json({ received: true });
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method not allowed');
+    return res.json({ received: true });
   }
+  res.setHeader('Allow', 'POST');
+  return res.status(405).end('Method not allowed');
 };
